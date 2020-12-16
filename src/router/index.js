0af9a8d3157a7ed8e8639b4e177894635ee39456
CedricNtwari <cedric.ntwari@powercoders.org>
2020-12-16 17:27:34 +0100

    Adding a navigation

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,22 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/blog',
+    name: 'Blog',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "blog" */ '../views/Blog.vue'),
+  },
+  {
+    path: '/works',
+    name: 'Works',
+    component: () => import(/* webpackChunkName: "works" */ '../views/Works.vue'),
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue'),
   },
 ]
 
