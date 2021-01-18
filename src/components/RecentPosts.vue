ce697a55504d57bb9eb8d7942d1a9af2cd769842
CedricNtwari <cedric.ntwari@powercoders.org>
2021-01-18 10:58:38 +0100

    Add recent posts to the homepage

---
+++ src/components/RecentPosts.vue
@@ -0,0 +1,56 @@
+<!-- This component creates a responsive navigation bar. -->
+<template>
+  <div class="recent-posts">
+    <div class="recent-posts__header">
+      <h3>Recent posts</h3>
+      <a href="/blog">View all</a>
+    </div>
+    <div class="recent-posts__blogPreview">
+      <BlogPreview
+        title="Making a design from from scratch"
+        infos="12 Feb 2020"
+        infos1="Design, Pattern"
+      />
+      <BlogPreview
+        title="Creating pixel perfect icons in Figma"
+        infos="12 Feb 2020"
+        infos1="Figma, Icon Design"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import BlogPreview from '@/components/BlogPreview.vue'
+
+export default {
+  name: 'RecentPosts',
+  components: {
+    BlogPreview,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.recent-posts {
+  background-color: #e3edee;
+
+  &__header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  &__header a {
+    text-decoration: none;
+    color: #1e81b0;
+  }
+}
+@media (min-width: 320px) and (max-width: 768px) {
+  .recent-posts {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="works">
     <Intro msg="Hi, I am Cédric, a creative front-end developer" />
+    <RecentPosts />
   </div>
 </template>
 
 <script>
 import Intro from '@/components/Intro.vue'
+import RecentPosts from '@/components/RecentPosts.vue'
 
 export default {
   name: 'Works',
   components: {
     Intro,
+    RecentPosts,
   },
 }
 </script>
