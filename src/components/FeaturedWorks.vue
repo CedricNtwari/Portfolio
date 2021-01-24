8bf5fc06e1a7283c38d4eadb454bf57f66d5a000
CedricNtwari <cedric.ntwari@powercoders.org>
2021-01-24 17:50:36 +0100

    Add featured works component

---
+++ src/components/FeaturedWorks.vue
@@ -0,0 +1,55 @@
+<!-- This component creates a FeaturedWorks to the homepage.. -->
+<template>
+  <div class="featured-works">
+    <h1 class="featured-works__header">Featured works</h1>
+    <WorkPreview
+      v-for="work in works"
+      :key="work.id"
+      :image="work.image"
+      :title="work.title"
+      :year="work.year"
+      :subject="work.subject"
+    />
+  </div>
+</template>
+
+<script>
+import WorkPreview from '@/components/WorkPreview.vue'
+
+export default {
+  name: 'FeaturedWorks',
+  components: {
+    WorkPreview,
+  },
+  data() {
+    return {
+      works: [
+        {
+          id: 1,
+          image: '../assets/dashboard.png',
+          title: 'Designing Dashboard',
+          year: '2020',
+          subject: 'Dashboard',
+        },
+        {
+          id: 2,
+          image: '../assets/portrait.jpg',
+          title: 'Vibrant Portraits of 2020',
+          year: '2018',
+          subject: 'Illustration',
+        },
+        {
+          id: 3,
+          image: '../assets/letterImage.jpg',
+          title: '36 Days od Malayalam type',
+          year: '2018',
+          subject: 'Typography',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/components/WorkPreview.vue
+++ src/components/WorkPreview.vue
@@ -2,11 +2,17 @@
 <template>
   <div class="work-preview">
     <div class="work-preview__column">
-      <img class="work-preview__image" /> alt="My work picture" src="" />
+      <img
+        class="work-preview__image"
+        alt="My work picture"
+        :src="image"
+        style="width:150px; height:150px;"
+      />
     </div>
     <div class="work-preview__column">
-      <h1 class="work-preview__header">>{{ title }}</h1>
-      <small class="work-preview__details">>{{ year }}{{ subject }} </small>
+      <h1 class="work-preview__header">{{ title }}</h1>
+      <small class="work-preview__year">{{ year }}</small>
+      <small class="work-preview__subject">{{ subject }}</small>
       <p class="work-preview__paragraph">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. A reprehenderit optio hic ullam
         temporibus aperiam consequuntur alias odio cupiditate praesentium maiores, ipsa neque?
@@ -20,6 +26,9 @@
 export default {
   name: 'WorkPreview',
   props: {
+    image: {
+      type: String,
+    },
     title: {
       type: String,
     },
