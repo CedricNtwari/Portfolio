e124fb6974d2c7073b95e9c696b2a6a92b30fef0
CedricNtwari <61406685+CedricNtwari@users.noreply.github.com>
2021-01-28 21:04:39 +0100

    Revert "Add featured works component to the homepage"

--- src/components/WorkPreview.vue
+++ src/components/WorkPreview.vue
@@ -2,12 +2,11 @@
 <template>
   <div class="work-preview">
     <div class="work-preview__column">
-      <img class="work-preview__image" alt="My work picture" :src="image" />
+      <img class="work-preview__image" /> alt="My work picture" src="" />
     </div>
     <div class="work-preview__column">
-      <h1 class="work-preview__header">{{ title }}</h1>
-      <small class="work-preview__year">{{ year }}</small>
-      <small class="work-preview__subject">{{ subject }}</small>
+      <h1 class="work-preview__header">>{{ title }}</h1>
+      <small class="work-preview__details">>{{ year }}{{ subject }} </small>
       <p class="work-preview__paragraph">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. A reprehenderit optio hic ullam
         temporibus aperiam consequuntur alias odio cupiditate praesentium maiores, ipsa neque?
@@ -21,9 +20,6 @@
 export default {
   name: 'WorkPreview',
   props: {
-    image: {
-      type: String,
-    },
     title: {
       type: String,
     },
@@ -38,31 +34,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-.work-preview {
-  // eslint-disable-next-line prettier/prettier
-  display: flex;
-  flex-wrap: wrap;
-
-  &:hover {
-    color: #1e81b0;
-  }
-
-  &__column {
-    flex: 50%;
-  }
-
-  &__image {
-    width: 100%;
-    max-width: 400px;
-  }
-}
-
-.work-preview::after {
-  display: inline-block;
-  content: '';
-  border-top: 1px solid lightgray;
-  width: 100%;
-  margin: 20px;
-}
-</style>
+<style scoped lang="scss"></style>
