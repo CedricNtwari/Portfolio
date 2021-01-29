0f663a31e0b3a1ad9b917af31e369f49bba00b08
CedricNtwari <cedric.ntwari@powercoders.org>
2021-01-29 21:11:03 +0100

    Align footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,16 @@
 <!-- This component adds the footer. -->
 <template>
   <div class="footer">
-    <i class="fab fa-facebook-square"></i>
-    <i class="fab fa-instagram-square"></i>
-    <i class="fab fa-twitter-square"></i>
-    <i class="fab fa-linkedin"></i>
+    <div class="footer__row">
+      <i class="fab fa-facebook fa-2x"></i>
+      <i class="fab fa-instagram fa-2x"></i>
+      <i class="fab fa-twitter fa-2x"></i>
+      <i class="fab fa-linkedin fa-2x"></i>
+    </div>
 
-    <p class="footer__copyright">Copyright @{{ currentYear }} All rights reserved</p>
+    <div class="footer__row">
+      <p class="footer__copyright">Copyright @{{ currentYear }} All rights reserved</p>
+    </div>
   </div>
 </template>
 
@@ -24,8 +28,30 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .footer {
-  display: flex;
-  justify-content: center;
-  padding: 40px 0 30px 0;
+  &__row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .fa-facebook {
+    list-style-type: none;
+    padding: 5px;
+    margin: 5px;
+  }
+  .fa-instagram {
+    list-style-type: none;
+    padding: 5px;
+    margin: 5px;
+  }
+  .fa-twitter {
+    list-style-type: none;
+    padding: 5px;
+    margin: 5px;
+  }
+  .fa-linkedin {
+    list-style-type: none;
+    padding: 5px;
+    margin: 5px;
+  }
 }
 </style>
