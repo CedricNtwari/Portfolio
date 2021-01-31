d2bfbb9c0c9c7353b06466579f0c104fc68292aa
CedricNtwari <cedric.ntwari@powercoders.org>
2021-01-31 19:19:44 +0100

    Add content Blog page

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="blog">
-    <h1>This is a Blog page</h1>
+    <h1>{{ header }}</h1>
+    <BlogPreview /><BlogPreview /><BlogPreview /><BlogPreview /><BlogPreview />
+    <Footer class="blog__footer" />
   </div>
 </template>
+
+<script>
+import BlogPreview from '@/components/BlogPreview.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Blog',
+  components: {
+    BlogPreview,
+    Footer,
+  },
+
+  data() {
+    return {
+      header: 'Blog',
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.blog {
+  &__footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+}
+</style>
