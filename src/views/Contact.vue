b23d2409cab86220e0cc47f79ba3d9d7a88c9af8
CedricNtwari <cedric.ntwari@powercoders.org>
2021-02-06 21:38:12 +0100

    Add HTML contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="contact">
-    <h1>This is a Contact page</h1>
+    <h1 class="contact__header">{{ header }}</h1>
+    <h6 class="contact__title">{{ title }}</h6>
+    <form action="" method="Post">
+      <div class="contact__block">
+        <label class="contact__label" for="name">Name</label>
+        <input class="contact__input" type="text" v-model="name" required />
+      </div>
+      <div class="contact__block">
+        <label class="contact__label" for="email">Email</label>
+        <input class="contact__input" type="email" v-model="email" required />
+      </div>
+      <div class="contact__block">
+        <label class="contact__label" for="textarea">Message</label>
+        <textarea class="contact__textarea" v-model="textarea"></textarea>
+      </div>
+      <div class="contact__button">
+        <button>Send message</button>
+      </div>
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Contact',
+
+  data() {
+    return {
+      header: 'Contact',
+      title: 'To get in touch fill the form below',
+    }
+  },
+}
+</script>
