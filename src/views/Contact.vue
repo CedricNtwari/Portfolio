d1fff7a301d220a9489585ff804f1e96f394ce03
CedricNtwari <cedric.ntwari@powercoders.org>
2021-02-09 22:27:56 +0100

    Validate form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,10 +2,11 @@
   <div class="contact">
     <h1 class="contact__header">{{ header }}</h1>
     <h6 class="contact__title">{{ title }}</h6>
-    <form action="" method="Post">
+    <form @submit.prevent="handleSubmit">
       <div class="contact__block">
         <label class="contact__label" for="name">Name</label> <br />
         <input class="contact__input" type="text" v-model="name" required />
+        <div v-if="nameError" class="contact__error">{{ nameError }}</div>
       </div>
       <div class="contact__block">
         <label class="contact__label" for="email">Email</label> <br />
@@ -13,7 +14,7 @@
       </div>
       <div class="contact__block">
         <label class="contact__label" for="textarea">Message</label> <br />
-        <textarea class="contact__textarea" v-model="textarea"></textarea>
+        <textarea class="contact__textarea" v-model="message"></textarea>
       </div>
       <div class="contact__button">
         <button>Send message</button>
@@ -30,7 +31,27 @@ export default {
     return {
       header: 'Contact',
       title: 'To get in touch fill the form below',
+      name: '',
+      email: '',
+      message: '',
+      nameError: '',
     }
+  },
+  methods: {
+    handleSubmit() {
+      console.log('form submitted')
+
+      // validate name
+      this.nameError = this.name.length > 1 ? '' : 'Name must be more than one character'
+    },
   },
 }
 </script>
+
+<style lang="scss">
+.contact {
+  &__error {
+    color: red;
+  }
+}
+</style>
