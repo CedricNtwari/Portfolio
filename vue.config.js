08e9dcfa9d412f402d34c04969e1ab0c8fc78f34
CedricNtwari <cedric.ntwari@powercoders.org>
2021-06-16 09:34:39 +0200

    Fix linting errors

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                additionalData: `
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
                 @import '@/styles/breakpoints.scss';
                 @import '@/styles/colors.scss';
           `,
-            },
-        },
+      },
     },
+  },
 }
